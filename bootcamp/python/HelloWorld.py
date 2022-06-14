12a50b330ee276492728b2eee795e07e98e805e7
jp <johnamata@protonmail.com>
2022-06-14 16:22:05 +0800

    grind

---
+++ bootcamp/python/HelloWorld.py
@@ -0,0 +1,2 @@
+print('Hello, World') # prints the string "hello world" to the console or standard output
+# the value inside print() is a string, denoted by ''
---
+++ bootcamp/python/Variable.py
@@ -0,0 +1,15 @@
+# variable = saves value for re-use, can be updated
+# variable naming convention: be descriptive, lower case, and separte words by underscores "_"
+
+hello_world = 'Hello, World ' # assign a value to a variable on right hand side of equal sign
+# hello_world is a variable with a string variable with a value assigned to it: 'Hello, World'
+
+print(hello_world) # we call the variable with the print() in-built function, which calls the assigned value to it, which is 'Hello, World ' thus it prints
+# 'Hello, World '
+
+hello_world = 'World, Hello' #assign a different value to hello_world variable, basically overwritten the variable
+print(hello_world) # now it doesnt print 'Hello, World ' but instead prints the new value, 'World, Hello
+
+# we can reference variables as well, for example
+
+hello_world2 = hello_world # hello_world2 contains the same value assigned to the variable hello_world. basically we  are referencing the variable hello_world
